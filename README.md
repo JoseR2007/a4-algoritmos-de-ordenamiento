32670203e02b3ea277767c8c13494c73501801fe
José Rafael <joserafaeltineog@gmail.com>
2026-03-13 12:27:24 -0400

    Added a basic functions: menu

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# a4-algoritmos-de-ordenamiento
+# A4: Algoritmos de ordenamiento y busqueda
+1. Algoritmos de búsquedas y ordenamiento.
+  a. Ordenamiento
+    i. Burbuja
+    ii. Inserción
+    iii. Selección
+    iv. Shell
+    v. QuickSort
+  b. Búsqueda
+    i. Secuencial
+    ii. Binaria
+2. Puntero a funciones – Aplicaciones.
--- main.c
+++ main.c
@@ -1,9 +1,55 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <conio.h>
+#include <string.h>
+
+// Teclas de control:
+#define ESC 27
+#define ENTER 13
+#define FLECHA 224
+#define BACKSPACE 8
+
+// Teclas de movimiento:
+#define ARRIBA 72
+#define ABAJO 80
+#define DERECHA 77
+#define IZQUIERDA 75
+
+int menu(char *, char *, int, int, int, int, int);
 
 int main()
 {
 
    return 0;
+}
+
+int menu(char *titulo, char *opciones, int cant_opciones, int posx, int posy, int color, int fondo)
+{
+   int ind = 0;
+   int tecla;
+
+   do
+   {
+      do
+      {
+         tecla = getch();
+      } while (tecla != ENTER && tecla != ESC && tecla != ARRIBA && tecla != ABAJO);
+
+      if (tecla == ARRIBA)
+      {
+         if (ind > 0)
+            ind--;
+         else if (ind == 0)
+            ind = cant_opciones - 1;
+      }
+      else if (tecla == ABAJO)
+      {
+         if (ind < cant_opciones - 1)
+            ind++;
+         else if (ind == cant_opciones - 1)
+            ind = 0;
+      }
+   } while (tecla != ENTER && tecla != ESC);
+
+   return ind;
 }
